3e68e334d1898b1e256f1bbbc80fd49226ca61a4
Asma-Chaouch <asma.chaouch@etudiant-fst.utm.tn>
2024-09-05 08:12:59 +0100

    with about page

--- src/components/acceuil.tsx
+++ src/components/acceuil.tsx
@@ -14,15 +14,54 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBagShopping, faBars, faXmark } from '@fortawesome/free-solid-svg-icons'; // Importer les icônes appropriées
 import 'aos/dist/aos.css';
 import './about.css'
+import { collection, getDocs } from 'firebase/firestore';
 import dileveryimg from '../assets/imgf/delivery-w.png';
-import backgroungimg from '../assets/imgf/background-1.png'
+import Headerprofile from './haeder';
 import illustraion1 from '../assets/imgf/illustration-1.png'
 import illustraion2 from '../assets/imgf/illustration-2.png'
 import illustraion3 from '../assets/imgf/illustration-3.png'
+import serviceicon1 from '../assets/imgf/service-icon-1.svg'
+import serviceicon2 from '../assets/imgf/service-icon-2.svg'
+import serviceicon3 from '../assets/imgf/service-icon-3.svg'
+import serviceicon4 from '../assets/imgf/service-icon-4.svg'
+import serviceicon5 from '../assets/imgf/service-icon-5.svg'
+import serviceicon6 from '../assets/imgf/service-icon-6.svg'
+import serviceicon7 from '../assets/imgf/service-icon-7.svg'
+import serviceicon8 from '../assets/imgf/service-icon-8.svg'
+import elements from '../assets/imgf/elements-1.jpg'
+import { db } from '../firebaseConfig';
 
-      const AboutPage: React.FC = () => {
+interface Command {
+  deliveryAddress: string;
+  deliveryCost: number;
+  deliveryManId: string;
+  discount: number;
+  finalTotal: number;
+  id: string;
+  items: any[];
+  paymentMethod: string;
+  status: string;
+  tip: number;
+}
+
+interface Restaurant {
+  name: string;
+  address: string;
+  available: boolean;
+}
+interface Client{
+  name: string;
+}
+
+    const AboutPage: React.FC = () => {
         const [loading, setLoading] = useState(true);
         const [isMenuOpen, setIsMenuOpen] = useState(false);
+        const [commandData, setCommandData] = useState<Command[]>([]);
+        const [restaurantData, setRestaurantData] = useState<Restaurant[]>([]);
+        const [commandCount, setCommandCount] = useState<number>(0);
+        const [restaurantCount, setRestaurantCount] = useState<number>(0);
+        const [clientData, setClientData] = useState<Client[]>([]);
+        const [clientCount, setClientCount] = useState<number>(0);
       
         useEffect(() => {
           AOS.init();
@@ -30,10 +69,41 @@ import illustraion3 from '../assets/imgf/illustration-3.png'
           const timer = setTimeout(() => setLoading(false), 2000); // Change time as needed
           return () => clearTimeout(timer);
         }, []);
+
+
+
+
+  useEffect(() => {
+    const fetchCommands = async () => {
+      const commandCollection = collection(db, 'command');
+      const commandSnapshot = await getDocs(commandCollection);
+      const commandList = commandSnapshot.docs.map(doc => doc.data() as Command);
+      setCommandData(commandList);
+      setCommandCount(commandList.length); // Compte des commandes
+    };
+
+    const fetchRestaurants = async () => {
+      const restaurantCollection = collection(db, 'shopData');
+      const restaurantSnapshot = await getDocs(restaurantCollection);
+      const restaurantList = restaurantSnapshot.docs.map(doc => doc.data() as Restaurant);
+      setRestaurantData(restaurantList);
+      setRestaurantCount(restaurantList.length); // Compte des restaurants
+    };
+
+    const fetchClients = async () => {
+      const clientCollection = collection(db, 'clients');
+      const clientSnapshot = await getDocs(clientCollection);
+      const clientList = clientSnapshot.docs.map(doc => doc.data() as Client);
+      setClientData(clientList);
+      setClientCount(clientList.length); // Compte des commandes
+    };
+
+    fetchCommands();
+    fetchRestaurants();
+    fetchClients();
+  }, []);
       
-        const toggleMenu = () => {
-          setIsMenuOpen(prevState => !prevState);
-        };
+
       
         return (
           <>
@@ -50,140 +120,8 @@ import illustraion3 from '../assets/imgf/illustration-3.png'
                 </div>
               </div>
             )}
-      <header>
-<div className="container">
-  <div className="row align-items-center">
-    <div className="col-xl-2">
-      <div className="header-style">
-        <Link to="/">
-          <svg xmlns="http://www.w3.org/2000/svg" width="163" height="38" viewBox="0 0 163 38">
-            <g id="Logo" transform="translate(-260 -51)">
-              <g id="Logo-2" data-name="Logo" transform="translate(260 51)">
-                <g id="Elements">
-                  <path id="Path_1429" data-name="Path 1429" d="M315.086,140.507H275.222v-.894c0-11.325,8.941-20.538,19.933-20.538s19.931,9.213,19.931,20.538Z" transform="translate(-270.155 -115.396)" fill="#f29f05" />
-                  <path id="Path_1430" data-name="Path 1430" d="M301.13,133.517a1.488,1.488,0,0,1-1.394-.994,11.361,11.361,0,0,0-10.583-7.54,1.528,1.528,0,0,1,0-3.055,14.353,14.353,0,0,1,13.37,9.527,1.541,1.541,0,0,1-.875,1.966A1.444,1.444,0,0,1,301.13,133.517Z" transform="translate(-264.176 -113.935)" fill="#fff" />
-                  <path id="Path_1431" data-name="Path 1431" d="M297.343,146.544a14.043,14.043,0,0,1-13.837-14.211h2.975a10.865,10.865,0,1,0,21.723,0h2.975A14.043,14.043,0,0,1,297.343,146.544Z" transform="translate(-266.247 -108.544)" fill="#363636" />
-                  <path id="Path_1432" data-name="Path 1432" d="M302.183,132.519a7.064,7.064,0,1,1-14.122,0Z" transform="translate(-264.027 -108.446)" fill="#363636" />
-                  <path id="Path_1433" data-name="Path 1433" d="M320.332,134.575H273.3a1.528,1.528,0,0,1,0-3.055h47.033a1.528,1.528,0,0,1,0,3.055Z" transform="translate(-271.815 -108.923)" fill="#f29f05" />
-                  <path id="Path_1434" data-name="Path 1434" d="M289.154,123.4a1.507,1.507,0,0,1-1.487-1.528v-3.678a1.488,1.488,0,1,1,2.975,0v3.678A1.508,1.508,0,0,1,289.154,123.4Z" transform="translate(-264.154 -116.667)" fill="#f29f05" />
-                  <path id="Path_1435" data-name="Path 1435" d="M284.777,138.133H275.3a1.528,1.528,0,0,1,0-3.055h9.474a1.528,1.528,0,0,1,0,3.055Z" transform="translate(-270.84 -107.068)" fill="#363636" />
-                  <path id="Path_1436" data-name="Path 1436" d="M284.8,141.691h-6.5a1.528,1.528,0,0,1,0-3.055h6.5a1.528,1.528,0,0,1,0,3.055Z" transform="translate(-269.379 -105.218)" fill="#363636" />
-                </g>
-              </g>
-              <text id="Quickeat" transform="translate(320 77)" fill="#363636" fontSize="20" fontFamily="Poppins" fontWeight="700">
-                <tspan x="0" y="0">QUICK</tspan>
-                <tspan y="0" fill="#f29f05">EAT</tspan>
-              </text>
-            </g>
-          </svg>
-        </Link>
-        <div className="extras bag">
-          <a href="javascript:void(0)" className="menu-btn" onClick={toggleMenu}>
-            <FontAwesomeIcon icon={faBagShopping} />
-          </a>
-          <div className="bar-menu" onClick={toggleMenu}>
-            <FontAwesomeIcon icon={faBars} />
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className="col-lg-7">
-<nav className="navbar">
-  <ul className="navbar-links">
-  <li className="navbar-dropdown">
-      <Link to="/indexpage">Home</Link>
-    </li>
-   
-    <li className="navbar-dropdown active">
-      <Link to="/about">About Us</Link>
-    </li>
-    <li className="navbar-dropdown">
-      <a href="#">Restaurants</a>
-      <div className="dropdown">
-        <Link to="/restaurants">Restaurants</Link>
-        <Link to="/restaurant-card">Restaurant Card</Link>
-        <Link to="/checkout">Checkout</Link>
-      </div>
-    </li>
-    <li className="navbar-dropdown">
-      <a href="#">Pages</a>
-      <div className="dropdown">
-        <Link to="/blog">Blog</Link>
-        <Link to="/single-blog">Single Blog</Link>
-        <Link to="/services">Services</Link>
-        <Link to="/faq">FAQ</Link>
-        <Link to="/pricing-table">Pricing Table</Link>
-        <Link to="/become-partner">Become A Partner</Link>
-        <Link to="/404">404</Link>
-      </div>
-    </li>
-    <li className="navbar-dropdown">
-      <Link to="/Contacts">Contacts</Link>
-    </li>
-   
-  </ul>
-</nav>
-</div>
-    <div className="col-lg-3">
-      <div className="extras bag">
-        <a href="javascript:void(0)" id="desktop-menu" className="menu-btn" onClick={toggleMenu}>
-          <FontAwesomeIcon icon={faBagShopping} />
-        </a>
-        <Link to="#" className="button button-2">Order Now</Link>
-      </div>
-    </div>
-    {isMenuOpen && (
-      <div className="menu-wrap">
-        <div className="menu-inner ps ps--active-x ps--active-y">
-          <span className="menu-cls-btn" onClick={toggleMenu}>
-            <i className="cls-leftright"></i>
-            <i className="cls-rightleft"></i>
-          </span>
-          <div className="checkout-order">
-            <div className="title-checkout">
-              <h2>My Orders</h2>
-            </div>
-            <div className="banner-wilmington">
-              <img alt="logo" src="https://via.placeholder.com/50x50" />
-              <h6>Kennington Lane Cafe</h6>
-            </div>
-            <ul>
-              <li className="price-list">
-                <FontAwesomeIcon icon={faXmark} className="closeButton" />
-                <div className="counter-container">
-                  <div className="counter-food">
-                    <img alt="food" src="https://via.placeholder.com/100x100" />
-                    <div className="content-food">
-                      <p>Brown Hamburger</p>
-                      <h6>Hamburgers</h6>
-                    </div>
-                  </div>
-                  <div className="food-price">
-                    <h6 className="text-red">$28.00</h6>
-                    <div className="food-count">
-                      <button className="counter-btn">-</button>
-                      <span className="counter-input">1</span>
-                      <button className="counter-btn">+</button>
-                    </div>
-                  </div>
-                </div>
-              </li>
-            </ul>
-            <div className="checkout">
-              <h5>Sub Total</h5>
-              <h5 className="text-red">$22.00</h5>
-            </div>
-            <div className="process-checkout">
-              <Link to="/checkout" className="button button-2">Order Now</Link>
-            </div>
-          </div>
-        </div>
-      </div>
-    )}
-  </div>
-</div>
-</header>
-<section className="hero-section gap" >
+            <Headerprofile/>
+<section className="hero-section gap">
 		<div className="container">
 			<div className="row align-items-center">
 				<div className="col-lg-6" data-aos="fade-up"  data-aos-delay="200" data-aos-duration="300">
@@ -191,7 +129,7 @@ import illustraion3 from '../assets/imgf/illustration-3.png'
 						<h2>Les Meilleurs Restaurnts Chez Vous</h2>
 						<p>Commandez vos plats préférés en quelques clics et laissez-nous nous occuper du reste!</p>
 						<div className="nice-select-one">
-						<a href="#" className="button button-2" style={{paddingTop: "4"}}>Choisir Votre restaurant</a>
+						<a href="/restaurants" className="my-button my-button-2" style={{paddingTop: "4"}}>Choisir Votre restaurant</a>
 						</div>
 					</div>
 				</div>
@@ -203,7 +141,7 @@ import illustraion3 from '../assets/imgf/illustration-3.png'
 			</div>
 		</div>
 	</section>
-	<section className="works-section gap no-top">
+	<section className="works-section gap no-top" style={{paddingBottom:"50px"}}>
 		<div className="container">
 			<div className="hading" data-aos="fade-up"  data-aos-delay="200" data-aos-duration="300">
 				<h2>  Comment ça marche</h2>
@@ -228,9 +166,51 @@ import illustraion3 from '../assets/imgf/illustration-3.png'
 				<div className="col-lg-4 col-md-6 col-sm-12" data-aos="flip-up"  data-aos-delay="400" data-aos-duration="500">
 					<div className="work-card">
 						<img alt="img" src={illustraion3}/>
-						<h4><span>03</span>Passez votre commande</h4>
+						<h4><span>03</span> Passez votre commande</h4>
 						<p>Passez commande facilement en quelques clics grâce à notre interface conviviale. Choisissez votre mode de paiement préféré et confirmez votre commande en toute sécurité.</p>
 					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+  <section className="about-counters-section gap" >
+		<div className="container">
+			<div className="row align-items-center">
+      <div className="col-lg-12 col-md-6 col-sm-6">
+						<div className="counter-hading">
+							<p className='my-about-text'>Le service fait preuve de bon goût</p>
+						</div>
+					</div>
+				<div className="col-lg-6 col-md-12 col-sm-12" data-aos="flip-up"  data-aos-delay="200" data-aos-duration="300">
+					<div className="about-counters-img">
+						<img alt="girl" src={elements} style={{borderRadius:"5px"}} />
+					</div>
+				</div>
+				<div className="col-lg-6 col-md-12 col-sm-12" data-aos="flip-up"  data-aos-delay="300" data-aos-duration="400">
+					<div className="row">
+					<div className="col-lg-6 col-md-6 col-sm-6">
+						<div className="count-time" style={{border:"2px solid #FF965A" , marginBottom:"30px" , alignItems:"center"}}>
+								<h2 className="timer count-title count-number" data-to="976" data-speed="2000">{clientCount}</h2>
+									<p>Client<br/>
+									Satisfait</p>
+						</div>
+				</div>
+				<div className="col-lg-6 col-md-6 col-sm-6" data-aos="flip-up"  data-aos-delay="300" data-aos-duration="400">
+						<div className="count-time" style={{border:"2px solid #FF965A" , alignItems:"center"}}>
+								<h2 className="timer count-title count-number" data-to="12" data-speed="2000">{restaurantCount}</h2>
+									<p>Meilleurs<br/>
+											Restaurants</p>
+						</div>
+				</div>
+				<div className="col-lg-6 col-md-6 col-sm-6">
+						<div className="count-time sp" style={{border:"2px solid #FF965A" , alignItems:"center"}}>
+								<h2 className="timer count-title count-number" data-to="1" data-speed="2000">{commandCount}</h2>
+								<span></span>
+									<p>Repas<br/>
+											Deliverée</p>
+						</div>
+				</div>
+			</div>
 				</div>
 			</div>
 		</div>
@@ -241,31 +221,40 @@ import illustraion3 from '../assets/imgf/illustration-3.png'
           {/* Repeat for each card */}
           <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12" data-aos="flip-up" data-aos-delay="200" data-aos-duration="300">
             <div className="card-text-data">
-              <img className="on" alt="icon" src="assets/img/service-icon-2.svg" />
-              <img className="off" alt="icon" src="assets/img/service-icon-1.svg" />
-              <h3>Free Delivery</h3>
-              <p>Cras fermentum odio eu feugiat pretium nibh ipsum. Ut faucibus pulvinar elementum integer enim neque volutpat.</p>
+              <img className="on" alt="icon" src={serviceicon2} />
+              <img className="off" alt="icon" src={serviceicon1} />
+              <h3>Satisfaction Garantie</h3>
+              <p>Un service irréprochable, de la commande à la livraison , Profitrez.</p>
+            </div>
+          </div>
+          <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12" data-aos="flip-up" data-aos-delay="200" data-aos-duration="300">
+            <div className="card-text-data">
+              <img className="on" alt="icon" src={serviceicon3} />
+              <img className="off" alt="icon" src={serviceicon4} />
+              <h3>À Table en un Clin d’Œil</h3>
+              <p>Votre repas livré en un temps record, toujours chaud et prêt à déguster</p>
+            </div>
+          </div>
+          <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12" data-aos="flip-up" data-aos-delay="200" data-aos-duration="300">
+            <div className="card-text-data">
+              <img className="on" alt="icon" src={serviceicon7} />
+              <img className="off" alt="icon" src={serviceicon8} />
+              <h3>Le Goût de l’Excellence</h3>
+              <p>Un Voyage Culinaire à Domicile, avec des plats savoureux et authentiques.</p>
+            </div>
+          </div>
+          <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12" data-aos="flip-up" data-aos-delay="200" data-aos-duration="300">
+            <div className="card-text-data">
+              <img className="on" alt="icon" src={serviceicon5} />
+              <img className="off" alt="icon" src={serviceicon6} />
+              <h3>Savourez Sans Compter</h3>
+              <p>Savourez une cuisine délicieuse sans vous ruiner grâce à nos offres pour tous les budgets.</p>
             </div>
           </div>
           {/* Add other cards similarly */}
         </div>
       </div>
     </section>
-      <section className="cards-section gap no-top">
-        <div className="container">
-          <div className="row">
-            <div className="col-xl-3 col-lg-4 col-md-6 col-sm-12" data-aos="flip-up" data-aos-delay="200" data-aos-duration="300">
-              <div className="card-text-data">
-                <img className="on" alt="icon" src="assets/img/service-icon-2.svg" />
-                <img className="off" alt="icon" src="assets/img/service-icon-1.svg" />
-                <h3>Free Delivery</h3>
-                <p>Cras fermentum odio eu feugiat pretium nibh ipsum. Ut faucibus pulvinar elementum integer enim neque volutpat.</p>
-              </div>
-            </div>
-            {/* Ajoutez d'autres cartes de manière similaire */}
-          </div>
-        </div>
-      </section>
       <footer>
         <Footer />
       </footer>
